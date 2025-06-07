865800a539a4762c5d42201805c67b29652b66d1
maasir554 <maasir554@gmail.com>
2025-06-07 21:20:01 +0530

    connection to postgres neondb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,38 @@
 import express from 'express'
-const app = express();
+import dotenv from 'dotenv'
+import { sql } from './config/db.js';
+dotenv.config();
+const PORT = process.env.PORT || 5111;
 
-app.listen(5111, ()=>{
-    console.log("Server running on PORT 5111\n")
-})
+const initDB = async () => {
+    try{
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY, 
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+        console.log("Database connection initialized successfully!");
+    }
+    catch(e){
+        console.log("Error in initializing connection with DB.\nError:",e);
+        process.exit(1);
+    }
+}
+
+const app = express();
 
 app.get("/",(req,res)=>{
     res.send("Hey, its working!! real crazy")
 })
+
+// start the express app only when the database is connected.
+initDB().then(
+    ()=>{
+        app.listen(PORT, ()=>{
+            console.log(`Server now running on PORT:${PORT}`)
+        })
+    }
+)

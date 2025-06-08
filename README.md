a026528171c4bb6cef19c74faadb1c75dcf653a1
maasir554 <maasir554@gmail.com>
2025-06-08 16:23:38 +0530

    api endpoint for recording tansactions

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ This is a fullstack expense-tracker cross-platform mobile application.
 - Database used: Postgres SQL via neondb 
 - No ORM used, only the using Raw SQL.
 - taggged template litral to call function
+- Clerk user management platform for managing auth and related stuff.
 ### new concept learnt 
+1. Tagged template litral function call
 ```js
 const fn = (x) => {
     // some function that accepts string as the parameter
@@ -15,4 +17,14 @@ const fn = (x) => {
 fn("Hello World");
 // Tagged litral way of calling:
 fn`Hello World`
+```
+2. Process exit code
+
+```js
+try{
+//...
+}catch(e){
+//...
+    process.exit(1)
+}
 ```
--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const initDB = async () => {
             created_at DATE NOT NULL DEFAULT CURRENT_DATE
         )`;
         console.log("Database connection initialized successfully!");
-    }
+    } // DECIMAL(10,2) 10 digits in total, and 2 digits after decimal 99999999.99
     catch(e){
         console.log("Error in initializing connection with DB.\nError:",e);
         process.exit(1);
@@ -23,9 +23,32 @@ const initDB = async () => {
 }
 
 const app = express();
+app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("Hey, its working!! real crazy")
+})
+
+app.post("/api/transactions", async (req,res) => {
+    try{
+        const {title, amount, category, user_id} = req.body;
+        
+        if(!title || !amount || ! category || !user_id) 
+        return res.status(400).json({message:"All fileds are required."});
+        
+        const transaction = await sql`
+            INSERT INTO transactions(user_id,title,amount,category)
+            VALUES(${user_id},${title},${amount},${category})
+            RETURNING *
+        `
+        console.log(transaction);
+        res.status(201).json(transaction[0]); // actually the sql function is gonna return a list of created values
+        // we know that there's gonna be a single entry as we prompted for.
+    }
+    catch(err){
+        console.log("Error in /api/transactions: ", err);
+        res.status(500).json({message: "Internal srver error.", endpoint:"/api/transactions"})
+    }
 })
 
 // start the express app only when the database is connected.

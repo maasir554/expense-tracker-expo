91168c119dcd9057e81db8617994f4602547c4b2
maasir554 <maasir554@gmail.com>
2025-06-09 17:14:46 +0530

    better folder structure backend

--- backend/src/middleware/RateLimiter.js
+++ backend/src/middleware/RateLimiter.js
@@ -1,7 +1,6 @@
 import ratelimiter from "../config/upstash.js";
 
 const RateLimiter = async (req, res, next) => {
-    console.log("Rate Limiter is checking... ")
     try{
         const { success } = await ratelimiter.limit("my-rate-limit"); // we will use userId or IP address here later.
         if(!success) return res.status(429).json({message:"Too many requests. Please try again after some time."});

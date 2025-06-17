e2ff5d9de04dd98a0c083c35de9ab82542925788
maasir554 <maasir554@gmail.com>
2025-06-17 17:26:44 +0530

    setup clerk authentication

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
 import { SafeScreen } from "@/components/SafeScreen";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { Slot } from 'expo-router'
 
 export default function RootLayout() {
-  return<SafeScreen><Stack screenOptions={{headerShown:false}}/></SafeScreen>
+  return(
+  <ClerkProvider tokenCache={tokenCache}>
+    <SafeScreen>
+      <Slot />
+      {/* <Stack screenOptions={{headerShown:false}}/> */}
+    </SafeScreen>
+  </ClerkProvider>
+  )
 }
--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -5,7 +5,7 @@ import {useSafeAreaInsets } from "react-native-safe-area-context";
 const SafeScreen = ({children}) => {
     const insets = useSafeAreaInsets()
     return(
-        <View style={{paddingTop:insets.top,flex:1,backgroundColor:COLORS.background}}>{children}</View>
+        <View style={{paddingTop:insets.top,paddingBottom:insets.bottom,flex:1,backgroundColor:COLORS.background}}>{children}</View>
     )
 }
 
